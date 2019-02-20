8b4cd736d5fe0128cceb54083a2b9a60d567317d
Gabcddrc <36075806+Gabcddrc@users.noreply.github.com>
2019-02-20 16:56:13 +0000

    Add files via upload

--- bloodTest/src/main/java/com/blood/dao/StaffDAO.java
+++ bloodTest/src/main/java/com/blood/dao/StaffDAO.java
@@ -2,8 +2,14 @@ package com.blood.dao;
 
 import com.blood.pojo.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StaffDAO extends JpaRepository<Staff, Long>{
     Staff findByUsername(String username);
     Staff findById(int id);
+    Staff findByEmail(int email);
+
+    //public Staff createStaff(Staff staff);
+    
 }
