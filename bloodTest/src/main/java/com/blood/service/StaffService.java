21f73e95689299014e8eab3d6b3730e4b4f25068
Gabcddrc <36075806+Gabcddrc@users.noreply.github.com>
2019-02-20 17:02:17 +0000

    Fix merge

--- bloodTest/src/main/java/com/blood/service/StaffService.java
+++ bloodTest/src/main/java/com/blood/service/StaffService.java
@@ -1,6 +1,5 @@
 package com.blood.service;
 
-
 import com.blood.dao.StaffDAO;
 import com.blood.pojo.Staff;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +14,42 @@ public class StaffService {
     private BCryptPasswordEncoder encoder;
 
     /*
-    Encode password
-    */
-    public void saveStaff(Staff staff){
+     * Encode password
+     */
+    public void saveStaff(Staff staff) {
         staff.setPassword(encoder.encode(staff.getPassword()));
         staffDAO.save(staff);
     }
 
     /*
-        change password
-    */
-    public void changePassword(Staff staff, String password){
+     * change password
+     */
+    public void changePassword(Staff staff, String password) {
         staff.setPassword(encoder.encode(password));
         staffDAO.save(staff);
     }
 
     /*
-        change username
-    */
-    public void changeUsername(Staff staff, String username){
-        staff.setUsername(username);;
+     * change username
+     */
+    public void changeUsername(Staff staff, String username) {
+        staff.setUsername(username);
+        ;
         staffDAO.save(staff);
     }
 
-    public Staff getById(int id){return staffDAO.findById(id);}
+    public Staff getById(int id) {
+        return staffDAO.findById(id);
+    }
+
     public Staff getByUsername(String username) {
         return staffDAO.findByUsername(username);
     }
 
+    public Staff createStaff(Staff staff){
+        //  Staff newStaff = new Staff(staff.getUsername(),staff.getForename(),staff.getSurname(), staff.getEmail(),
+        //   encoder.encode(staff.getPassword()), encoder.encode(staff.getConfirmPassword()));
+         staffDAO.save(staff);
+         return staff;
+     }
 }
