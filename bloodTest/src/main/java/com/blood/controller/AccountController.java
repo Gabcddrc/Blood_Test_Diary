40d1446aceabb9c15fe7c08d30346a489453c53b
Gabcddrc <seedjeff@hotmail.com>
2019-02-21 17:36:27 +0000

    Add encryption

--- bloodTest/src/main/java/com/blood/controller/AccountController.java
+++ bloodTest/src/main/java/com/blood/controller/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Controller
 public class AccountController {
@@ -22,38 +22,45 @@ public class AccountController {
     StaffService staffService;
 
     StaffValidator staffValidator;
-    
-    @RequestMapping(value="/login")
+
+    @RequestMapping(value = "/login")
     public String getLogin(Model model, String error) {
-        if (error != null)
-        {model.addAttribute("error", "Your username and password is invalid.");}
+        if (error != null) {
+            model.addAttribute("error", "Your username and password is invalid.");
+        }
 
         return "login";
     }
 
-   @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String registerForm(Model model) {
-       model.addAttribute("staff", new Staff());
+        model.addAttribute("staff", new Staff());
 
-       return "registers";
-   }
+        return "registers";
+    }
+
+    public static String encryption(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashed = passwordEncoder.encode(password);
+        return hashed;
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public String saveRegister(@ModelAttribute("staff") Staff staff, BindingResult bindingResult, Model model) {
-        //staffValidator.validate(staff, bindingResult);
-        Staff newStaff = new Staff(staff.getUsername(),staff.getForename(),staff.getSurname(), staff.getEmail(),
-          staff.getPassword());
+        // staffValidator.validate(staff, bindingResult);
+        Staff newStaff = new Staff(staff.getUsername(), staff.getForename(), staff.getSurname(), staff.getEmail(),
+                encryption(staff.getPassword()));
         System.out.print(staff.getUsername());
         if (bindingResult.hasErrors()) {
             return "registers";
         }
-        try{
-        staffService.createStaff(newStaff);}
-        catch(Exception e){
+        try {
+            staffService.createStaff(newStaff);
+        } catch (Exception e) {
             return "registers";
         }
 
         return "redirect:/home";
     }
-    
+
 }
