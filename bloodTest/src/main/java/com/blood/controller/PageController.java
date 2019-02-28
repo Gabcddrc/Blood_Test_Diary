2d11b47967d0201ac45efe34e67406da6318aab4
Gabcddrc <seedjeff@hotmail.com>
2019-02-28 01:34:36 +0000

    Fix navigation

--- bloodTest/src/main/java/com/blood/controller/PageController.java
+++ bloodTest/src/main/java/com/blood/controller/PageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
     @GetMapping(value="/")
     public String index(){
-        return "redirect:patients";
+        return "redirect:home";
     }
    
     /*@GetMapping(value="/home")
