6ee1e0b7708bb6e280d5ddfd98dfe768e16f94c1
Gabcddrc <seedjeff@hotmail.com>
2019-02-28 17:22:26 +0000

    Implement logout

--- bloodTest/src/main/java/com/blood/config/SecurityConfiguration.java
+++ bloodTest/src/main/java/com/blood/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.blood.service.StaffService;
 import com.blood.service.StaffDetailService;
@@ -44,6 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                     .permitAll()
                 .   and()
                 .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
                     .permitAll();
         
     }
