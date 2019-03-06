ac480e83a008617571467235b22d15df00c07175
Yeshvanth Prabakar <yeshvanth.prabakar@kcl.ac.uk>
2019-03-06 21:08:34 +0000

    Add Test for COntactsTest and PatientsTest POJO

--- bloodTest/src/test/java/com/blood/PatientTests.java
+++ bloodTest/src/test/java/com/blood/PatientTests.java
@@ -20,7 +20,6 @@ public class PatientTests {
     public void testGetForename() {
         
         Patient patient = new Patient("tForename", "tSurname", "tEmail");
-
         assertEquals(patient.getForename(), "tForename");
     }
 
@@ -28,9 +27,7 @@ public class PatientTests {
     public void testSetForename() {
         
         Patient patient = new Patient("tForename", "tSurname", "tEmail");
-
         patient.setForename("changedName");
-
         assertEquals(patient.getForename(), "changedName");
     }
 
@@ -38,7 +35,6 @@ public class PatientTests {
     public void testGetSurname() {
         
         Patient patient = new Patient("tForename", "tSurname", "tEmail");
-
         assertEquals(patient.getSurname(), "tSurname");
     }
 
@@ -46,9 +42,7 @@ public class PatientTests {
     public void testSetSurename() {
         
         Patient patient = new Patient("tForename", "tSurname", "tEmail");
-
         patient.setSurname("changedName");
-
         assertEquals(patient.getSurname(), "changedName");
     }
 
@@ -56,7 +50,6 @@ public class PatientTests {
     public void testGetEmail() {
         
         Patient patient = new Patient("tForename", "tSurname", "tEmail");
-
         assertEquals(patient.getEmail(), "tEmail");
     }
 
@@ -64,10 +57,74 @@ public class PatientTests {
     public void testSetEmail() {
         
         Patient patient = new Patient("tForename", "tSurname", "tEmail");
-
         patient.setEmail("changedEmail");
-
         assertEquals(patient.getEmail(), "changedEmail");
     }
+
+    @Test
+    public void testGetSurgery() {
+        
+        Patient patient = new Patient("tForename", "tSurname", "1997-03-03", 'M', "Great Britannia Street","tDiagnosis",
+       "tTransplant", "tlocal_hospital", "tSurgery", "tComments");
+        
+        assertEquals(patient.getSurgery(), "tSurgery");
+    }
+
+    @Test
+    public void testSetSurgery() {
+        
+        Patient patient = new Patient("tForename", "tSurname", "1997-03-03", 'M', "Great Britannia Street","tDiagnosis",
+        "tTransplant", "tlocal_hospital", "tSurgery", "tComments");
+        patient.setSurgery("changedSurgery");
+        assertEquals(patient.getSurgery(), "changedSurgery");
+    }
+
+    @Test
+    public void testGetComments() {
+        
+        Patient patient = new Patient("tForename", "tSurname", "1997-03-03", 'M', "Great Britannia Street","tDiagnosis",
+       "tTransplant", "tlocal_hospital", "tSurgery", "tComments");
+        
+        assertEquals(patient.getComments(), "tComments");
+    }
+
+    @Test
+    public void testSetComments() {
+        
+        Patient patient = new Patient("tForename", "tSurname", "1997-03-03", 'M', "Great Britannia Street","tDiagnosis",
+        "tTransplant", "tlocal_hospital", "tSurgery", "tComments");
+        patient.setComments("changedComments");
+        assertEquals(patient.getComments(), "changedComments");
+    }
+
+    @Test
+    public void testgetDiagnosis() {
+        
+        Patient patient = new Patient("tForename", "tSurname", "1997-03-03", 'M', "Great Britannia Street","tDiagnosis",
+       "tTransplant", "tlocal_hospital", "tSurgery", "tComments");
+        
+        assertEquals(patient.getDiagnosis(), "tDiagnosis");
+    }
+
+    @Test
+    public void testSetDiagnosis() {
+        
+        Patient patient = new Patient("tForename", "tSurname", "1997-03-03", 'M', "Great Britannia Street","tDiagnosis",
+        "tTransplant", "tlocal_hospital", "tSurgery", "tComments");
+        patient.setDiagnosis("changedDiagnosis");
+        assertEquals(patient.getDiagnosis(), "changedDiagnosis");
+    }
+
+    @Test
+    public void testEmptyDetails() {   
+        Patient patient = new Patient();
+        assertEquals(patient.getEmail(), null);
+        assertEquals(patient.getSurname(), null);
+        assertEquals(patient.getForename(), null);
+        assertEquals(patient.getSurgery(), null);
+        assertEquals(patient.getComments(), null);
+        assertEquals(patient.getDiagnosis(), null);
+    }
+    
 
 }
