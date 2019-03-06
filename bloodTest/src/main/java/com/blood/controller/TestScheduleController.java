fabf6d87503e3ef9f105bad8ab43d43df846cca9
Yeshvanth Prabakar <yeshvanth.prabakar@kcl.ac.uk>
2019-03-06 19:01:47 +0000

    Retrieve Label ID for Patientlabel

--- bloodTest/src/main/java/com/blood/controller/TestScheduleController.java
+++ bloodTest/src/main/java/com/blood/controller/TestScheduleController.java
@@ -1,17 +1,20 @@
 package com.blood.controller;
 
 import com.blood.pojo.Patient;
+import com.blood.pojo.TestSchedule;
 import com.blood.service.PatientService;
 import com.blood.service.TestScheduleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestScheduleController{
@@ -33,6 +36,23 @@ public class TestScheduleController{
         model.addAttribute("tests", this.tScheduleService.getAllTestSchedule());
         return "email";
     }
+
+    @RequestMapping(value = "/editLabel", method = RequestMethod.POST)
+    public String editLabel(@RequestParam(value = "checkboxName", required = false) String[] checkboxValue,Model model) {
+        if(checkboxValue[0] != null)
+        {
+         
+          for (String var : checkboxValue) {
+            System.out.println(var);
+          }
+        }
+        else
+        {
+          System.out.println("checkbox is not checked");
+        }
+        return "redirect:/home";
+    }
+
 
 
 }
