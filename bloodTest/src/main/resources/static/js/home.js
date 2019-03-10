647cb8cdee1a4b46713c30e5e808aae08e4bab6e
Gabcddrc <seedjeff@hotmail.com>
2019-03-10 17:15:31 +0000

    Implement filter 1.0

--- bloodTest/src/main/resources/static/js/home.js
+++ bloodTest/src/main/resources/static/js/home.js
@@ -1,3 +1,21 @@
+function filter(fill) {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("flag");
+  filter = fill.toUpperCase();
+  table = document.getElementById("testTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[9];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 function searchTestFunction() {
     var input, filter, table, tr, td, i;
     input = document.getElementById("patient");
