44629c62a0ea026688edebd4a6fc708154343e73
Gabcddrc <seedjeff@hotmail.com>
2019-03-12 14:16:21 +0000

    Add more filter

--- bloodTest/target/classes/static/js/home.js
+++ bloodTest/target/classes/static/js/home.js
@@ -1,11 +1,11 @@
-function filter(fill) {
+function filterFlag(fill) {
   var input, filter, table, tr, td, i;
   input = document.getElementById("flag");
   filter = fill.toUpperCase();
   table = document.getElementById("testTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[8];
+    td = tr[i].getElementsByTagName("td")[7];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
@@ -16,29 +16,71 @@ function filter(fill) {
   }
 }
 
-function searchTestFunction() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("patient");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("testTable");
-    tr = table.getElementsByTagName("tr");
+function filterComplete(fill) {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("completed");
+  filter = fill.toUpperCase();
+  table = document.getElementById("testTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      if (td.innerHTML.toUpperCase().includes(filter)) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterMissTest(fill) {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("missTest");
+  filter = fill.toUpperCase();
+  table = document.getElementById("testTable");
+  tr = table.getElementsByTagName("tr");
+  if (filter != "") {
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[2];
+      td = tr[i].getElementsByTagName("td")[6];
       if (td) {
-        if (td.innerHTML.toUpperCase().includes(filter)) {
+        if(filter == "1" && td.innerHTML.toUpperCase() == "0")
+        {
           tr[i].style.display = "";
-        } else {
+        } 
+        else if(filter == "2" && td.innerHTML.toUpperCase() != "0"){
+          tr[i].style.display = "";
+        }
+        else {
           tr[i].style.display = "none";
         }
       }
     }
   }
+}
+
+function searchTestFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("patient");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("testTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      if (td.innerHTML.toUpperCase().includes(filter)) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 
 $(document).ready(function () {
-    $("#1").click(function () {
-      $("#red").click(function () {
-        document.getElementById("b1").className = "badge red";
-      });
+  $("#1").click(function () {
+    $("#red").click(function () {
+      document.getElementById("b1").className = "badge red";
     });
   });
-
+});
