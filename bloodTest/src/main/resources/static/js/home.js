fab800fe3cebba80db7a568568bf243bc73aea22
Gabcddrc <seedjeff@hotmail.com>
2019-03-13 23:11:30 +0000

    Fix search

--- bloodTest/src/main/resources/static/js/home.js
+++ bloodTest/src/main/resources/static/js/home.js
@@ -60,15 +60,15 @@ function filterMissTest(fill) {
 }
 
 function searchTestFunction() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("patient");
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("name");
   filter = input.value.toUpperCase();
   table = document.getElementById("testTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[2];
     if (td) {
-      if (td.innerHTML.toUpperCase().includes(filter)) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
